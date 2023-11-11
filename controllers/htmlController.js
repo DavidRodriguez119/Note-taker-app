35abf94ca53f272a8fa91e577dd3d84dbd30a2de
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-11-10 20:52:05 -0500

    added server file and htmlcontrollers

---
+++ controllers/htmlController.js
@@ -0,0 +1,15 @@
+const express = require(`express`);
+const router = express.Router();
+const path = require(`path`);
+
+// Show notes page
+router.get(`/notes`, (req,res) => {
+    res.sendFile(path.join(__dirname, `../public/notes.html`))
+});
+
+// Show index page
+router.get(`/`, (req,res) => {
+    res.sendFile(path.join(__dirname, `../public/index.html`))
+});
+
+module.exports = router;
